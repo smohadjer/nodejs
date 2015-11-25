3dec833b218736b1aff6c841afb4c7593e326ce6
Saeid Mohadjer <saeid@fastmail.fm>
2015-11-25 08:22:33 +0100

    added todo app

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var http = require('http');
 var server = http.createServer(function (req, res) {
-  res.write('Hello World');
-  res.end();
+	var body = '<h1>Hello World</h1>';
+	res.setHeader('Content-Type', 'text/html');
+	res.statusCode = 302;
+	//res.write(body);
+	res.end(body);
 });
 server.listen(3000);
-
